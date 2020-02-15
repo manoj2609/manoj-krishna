ef9bf19d5da00fb33c739a7c3551610a7f5ec164
manoj2609 <58427809+manoj2609@users.noreply.github.com>
2020-02-15 17:39:04 +0530

    Add files via upload

---
+++ bubble.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a[5],i,j;
+    printf("\nenter 5 numbers:");
+    for(i=0;i<5;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<4;i++)
+        for(j=4;j>i;j--)
+        if(a[j]<a[j-1]){         // if a[j]>a[j-1] it gives desending
+        int t=a[j];
+        a[j]=a[j-1];
+        a[j-1]=t;
+    }
+    printf("\nsorted list is:\n");
+    for(i=0;i<5;i++)
+    printf("%d\n",a[i]);
+    return 0;
+}
